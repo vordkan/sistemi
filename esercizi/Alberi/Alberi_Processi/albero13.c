d12586a5b8fa7120f82bb6509e5e21c482019a47
vordkan <mattiasenneca@gmail.com>
2024-05-14 09:36:54 +0200

    prova

--- esercizi/Alberi/Alberi_Processi/albero13.c
+++ esercizi/Alberi/Alberi_Processi/albero13.c
@@ -8,20 +8,31 @@ int glob=4;
 int pid = 0;
 int i=0;
 int main(){
+		printf("Sono %d figlio di %d. Glob: %d, Pid: %d\n\n",getpid(),getppid(),glob, pid);
+
 	fork();
 	glob--;
+		printf("Sono %d figlio di %d. Glob: %d, Pid: %d\n\n",getpid(),getppid(),glob, pid);
+
 	if (fork()){
 		glob--;
+			printf("Sono %d figlio di %d. Glob: %d, Pid: %d\n\n",getpid(),getppid(),glob, pid);
+
 	}
 	if (pid==0){
 		for (i=1;i<2;i++){
 			pid = fork();
 			glob++;
+				printf("Sono %d figlio di %d. Glob: %d, Pid: %d\n\n",getpid(),getppid(),glob, pid);
+
 		}
 	}
 	if (pid==0)
 		glob++;
+			printf("Sono %d figlio di %d. Glob: %d, Pid: %d\n\n",getpid(),getppid(),glob, pid);
+
 	if (!fork())
+		printf("Sono %d figlio di %d. Glob: %d, Pid: %d\n\n",getpid(),getppid(),glob, pid);
+
 		glob--;
-	printf("Sono %d figlio di %d. Glob: %d, Pid: %d\n\n",getpid(),getppid(),glob, pid);
 }
